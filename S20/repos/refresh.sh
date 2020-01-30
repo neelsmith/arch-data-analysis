ddf0dc72874b74c398a06b4419b3d045d75fa1e6
Neel Smith <dnsmith.neel@gmail.com>
2020-01-30 06:51:25 -0500

    Add repo loading script

---
+++ S20/repos/refresh.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env /bin/bash
+#
+# Read list of github repos, git clone or git pull as needed.
+#
+export GIT=`which git`
+export PWD=`which pwd`
+export LS=`which ls`
+export CP=`which cp`
+export CAT=`which cat`
+
+export ROOT=`pwd`
+
+for REPONAME in $(cat repos.txt) ; do
+  REPOPATH=https://github.com/$REPONAME/clas299
+  echo $REPOPATH
+  if [ ! -d $REPONAME ]
+  then
+    echo "Making directory " $REPONAME
+    mkdir $REPONAME
+    echo "Cloning " $REPOPATH
+    (cd $ROOT/$REPONAME && $GIT clone $REPOPATH)
+  else
+    echo "Pulling in " $ROOT/$DIR
+    (cd $ROOT/$REPONAME && $GIT pull)
+  fi
+  cd $ROOT
+done;
